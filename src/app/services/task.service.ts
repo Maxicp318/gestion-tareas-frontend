86f7401b8394523f66991a686b995d116ffd6cfe
Carlos Maximiliano Perez <87154912+Maxicp318@users.noreply.github.com>
2025-10-03 18:38:30 -0300

    Update task.service.ts
    Agregando url nueva de la api

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -8,7 +8,7 @@ import { Task } from '../models/task';
 })
 export class TaskService {
 
-  private url: string = "https://app-task-api-13.onrender.com";
+  private url: string = "https://app-tasks-api.onrender.com/";
 
   constructor(private http: HttpClient) { }
 
